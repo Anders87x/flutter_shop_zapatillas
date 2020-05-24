efa0d76e5a5a20e8cf84a667d9ccfedd4eb76601
Anderson <davis_anderson_87@hotmail.com>
2020-05-24 11:22:00 -0500

    24/05/2020 subiendo a github

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_shop_zapatillas/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Shop Zapatillas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
